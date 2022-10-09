3b222ab1f4f4f4eccc2375d3cfcf804aa91ee1b3
scarpas3 <scarpas3@tcnj.edu>
2022-10-09 18:23:26 -0400

    edited home screen text

--- src/db/schema.rb
+++ src/db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_13_190602) do
+ActiveRecord::Schema.define(version: 2022_10_09_204549) do
 
   create_table "app_configs", force: :cascade do |t|
     t.string "name"
     t.string "logo_url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "industry"
+    t.string "country"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
