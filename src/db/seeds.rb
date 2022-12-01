67ba193616ea0ae0e605e90843ddb61e85eb58a5
scarpas3 <scarpas3@tcnj.edu>
2022-12-01 17:43:26 -0500

    seeding database

--- src/db/seeds.rb
+++ src/db/seeds.rb
@@ -22,6 +22,14 @@ if Rails.env.development?
   admin = User.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
   user = User.create!(email: 'user@example.com', password: 'password', password_confirmation: 'password')
   company0 = Company.create!(id: 0) #company0 is used for association when initializing events
+  monsanto = Company.create!(id: 1, name: "Monsanto", description: "Monsanto is an agricultural company known for sueing farmers for using their seeds.", industry: "Agriculture", country: "USA") 
+  dupont = Company.create!(id: 2, name: "Dupont", description: "Dupont is a chemical company notorious for its work with Teflon, a chemical which was later found to be poisonous.", industry: "Chemicals", country: "USA")
+  mf = Event.create!(title: "Founding", description: "Monsanto is founded in 1901",  year: 1901, company_id: 1)
+  df = Event.create!(title: "Founding", description: "Dupont is founded in 1802",  year: 1802, company_id: 2)
+  ao = Event.create!(title: "Agent Orange", company_id: 1)
+  ao = Event.create!(title: "2019 Herbicide Lawsuit", company_id: 1)
+  tl = Event.create!(title: "Teflon Invented", company_id: 2)
+  df = Event.create!(title: "Teflon Lawsuit", company_id: 2)
 elsif Rails.env.production?
 
   # We're in a production application! Stay secure! No accounts with password!
